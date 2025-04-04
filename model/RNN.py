861d7c30cbed65bec6a68fea2fb231622433d748
PE <pierre-emmanuel.marrel@etu.univ-lyon1.fr>
2025-04-04 14:05:26 +0200

    Add new method with lstm (not finish) - MultiTasks.ipynb - model/RNN.py
    Update LSTM Gen
    - LSTMGen.ipynb

--- model/RNN.py
+++ model/RNN.py
@@ -74,45 +74,89 @@ class LSTM_GenText(nn.Module):
         output, (hidden_out, mem_out) = self.lstm(input_embs, (hidden_in, mem_in))
                 
         return self.mlp_out(output), hidden_out, mem_out
+    
+class LSTM_Multi_Tasks(nn.Module):
+    def __init__(self, num_emb, output_size, tasks:int, num_layers=1, hidden_size=128, dropout=0.5):
+        super(LSTM_Multi_Tasks, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.num_emb = num_emb
+        self.output_size = output_size
+
+        # Create an embedding layer to convert token indices to dense vectors
+        self.embedding = nn.Embedding(num_emb, hidden_size)
+        
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, 
+                            num_layers=num_layers, batch_first=True, dropout=dropout)
+        
+        for i in range(tasks):
+            setattr(self, f'fc_out_{i}', nn.Linear(hidden_size, output_size))
 
 
-def train_LSTM(model, train_loader, optimizer, loss_func, nb_epochs, validate_loader):
-    device = model.fc_out.weight.device
+    def forward(self, input_seq, hidden_in, mem_in, task:int):
+        """
+        Forward pass of the LSTM model.
+        This method takes an input sequence, hidden state, and memory state,
+        and returns the output of the LSTM model for a specific task.
+        
+        Args:
+            input_seq (_type_): _input sequence of shape (batch_size, seq_len)_
+            hidden_in (_type_): _initial hidden state of shape (num_layers, batch_size, hidden_size)_
+            mem_in (_type_): _initial memory state of shape (num_layers, batch_size, hidden_size)_
+            task (int): _task index to select the output layer_
+            
+        Returns:
+            _type_: _output of the LSTM model for the specified task_
+        """
+        # Convert token indices to dense vectors
+        input_embs = self.embedding(input_seq)
 
-    for epoch in range(nb_epochs):
-        model.train()
-        train_acc = 0
-        test_acc = 0
-        total_loss = 0
-        steps = 0
-        for i, (inputs, targets) in enumerate(train_loader):
-            bs = targets.shape[0]
+        # Pass the embeddings through the LSTM layer
+        output, (hidden_out, mem_out) = self.lstm(input_embs, (hidden_in, mem_in))
+        
+        # Pass the LSTM output through the fully connected layer to get the final output
+        fc_out = getattr(self, f'fc_out_{task}')
+        return fc_out(output), hidden_out, mem_out
 
-            # Initialize hidden and memory states
-            hidden = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
-            memory = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
+
+# def train_LSTM(model, train_loader, optimizer, loss_func, nb_epochs, validate_loader):
+#     device = model.fc_out.weight.device
+
+#     for epoch in range(nb_epochs):
+#         model.train()
+#         train_acc = 0
+#         test_acc = 0
+#         total_loss = 0
+#         steps = 0
+#         for i, (inputs, targets) in enumerate(train_loader):
+#             bs = targets.shape[0]
+
+#             # Initialize hidden and memory states
+#             hidden = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
+#             memory = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
             
-            pred, _, _ = model(inputs, hidden, memory)
+#             pred, _, _ = model(inputs, hidden, memory)
             
-            loss = loss_func(pred[:, -1, :], targets)
+#             loss = loss_func(pred[:, -1, :], targets)
             
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#             total_loss += loss.item()
             
-            train_acc += (pred[:, -1, :].argmax(1) == targets).sum()
-            steps += bs
+#             train_acc += (pred[:, -1, :].argmax(1) == targets).sum()
+#             steps += bs
             
-        if validate_loader is not None:
-            model.eval()
-            with torch.no_grad():
-                for inputs, targets in validate_loader:
-                    bs = targets.shape[0]
-                    hidden = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
-                    memory = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
-                    pred, _, _ = model(inputs, hidden, memory)
-                    test_acc += (torch.argmax(pred[:, -1, :], dim=1) == targets).sum().item()
-            test_acc /= len(validate_loader.dataset)
+#         if validate_loader is not None:
+#             model.eval()
+#             with torch.no_grad():
+#                 for inputs, targets in validate_loader:
+#                     bs = targets.shape[0]
+#                     hidden = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
+#                     memory = torch.zeros(model.num_layers, bs, model.hidden_size, device=device)
+#                     pred, _, _ = model(inputs, hidden, memory)
+#                     test_acc += (torch.argmax(pred[:, -1, :], dim=1) == targets).sum().item()
+#             test_acc /= len(validate_loader.dataset)
             
-    return train_acc, test_acc
+#     return train_acc, test_acc
